4b8e1019515ab47d756bb2bae725a11be51a5aa1
Gene Rhee <gkr6sy@virginia.edu>
2015-09-20 15:03:53 -0400

    added camera functionality

--- app/src/main/java/cs4720/cs/virginia/edu/locationlist/toDO.java
+++ app/src/main/java/cs4720/cs/virginia/edu/locationlist/toDO.java
@@ -1,13 +1,21 @@
 package cs4720.cs.virginia.edu.locationlist;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class toDO extends AppCompatActivity {
+
+    private Button cameraButton;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,14 @@ public class toDO extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        image = (ImageView)findViewById(R.id.imageV);
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                activateCamera();
+            }
+        });
        /* TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
@@ -49,5 +65,19 @@ public class toDO extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void activateCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap pic = (Bitmap) data.getExtras().get("data");
+        image.setImageBitmap(pic);
+        Log.d("pics", image.getScaleType().toString());
+        image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+    }
 
 }
