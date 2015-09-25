d761bd1ca31fcf5f6cacdfa0d19bdc35d2f70e3b
Gene Rhee <gkr6sy@virginia.edu>
2015-09-25 14:48:56 -0400

    started work on a ListFragment. Implements a skeleton save button. Redid design on todo.xml

--- app/src/main/java/cs4720/cs/virginia/edu/locationlist/toDO.java
+++ app/src/main/java/cs4720/cs/virginia/edu/locationlist/toDO.java
@@ -8,28 +8,42 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class toDO extends AppCompatActivity {
+public class toDO extends AppCompatActivity implements TaskFragment.OnFragmentInteractionListener{
 
 
     private Button cameraButton;
+    private Button saveButton;
+    private Bitmap picture;
     ImageView image;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("toDo", "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView txtView = (TextView) findViewById(R.id.oneItem);
         txtView.setText(message);
+        if (picture != null) {
+            image.setImageBitmap(picture);
+        } else {
+            image = (ImageView) findViewById(R.id.imageV);
+        }
 
-
-        image = (ImageView)findViewById(R.id.imageV);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                save();
+            }
+        });
         cameraButton = (Button) findViewById(R.id.cameraButton);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +59,7 @@ public class toDO extends AppCompatActivity {
         // Set the text view as the activity layout
         setContentView(textView);
         */
-
-
+        //adapter = new ArrayAdapter(this, )
     }
 
     @Override
@@ -80,11 +93,33 @@ public class toDO extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap pic = (Bitmap) data.getExtras().get("data");
-        image.setImageBitmap(pic);
+        picture = (Bitmap) data.getExtras().get("data");
+        image.setImageBitmap(picture);
         Log.d("pics", image.getScaleType().toString());
         image.setScaleType(ImageView.ScaleType.FIT_CENTER);
     }
 
+    @Override
+    protected void onDestroy() {
+        Log.i("toDo", "onDestroy Called");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        //savedInstanceState.putString();
+    }
+
+    private void save() {
+        /*
+        TODO save stuff
+        This is where I plan to do the database stuff
+         */
+    }
+
+    public void onFragmentInteraction(String id) {
+
+    }
 
 }
